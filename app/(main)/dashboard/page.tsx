8511f3d6ad89f47f4ab4bcd96aec863299eee0b5
Aarush Kumar <2005aarushkumar@gmail.com>
2025-07-22 13:09:07 +0530

    feat: add NeonDB with Drizzle ORM

---
+++ app/(main)/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const DashboardPage = () => {
+    return (
+        <div>
+            <h1 className="flex items-center justify-center mt-24 font-bold text-3xl">
+                Dashboard
+            </h1>
+        </div>
+    );
+};
+
+export default DashboardPage;
---
+++ lib/schema.ts
@@ -0,0 +1,53 @@
+import {
+    pgTable,
+    text,
+    varchar,
+    timestamp,
+    boolean,
+    uuid,
+    numeric,
+} from "drizzle-orm/pg-core";
+
+// Store Clerk's user data
+export const users = pgTable("users", {
+    id: uuid("id").primaryKey(), // Clerk's UUID as primary key
+    email: varchar("email", { length: 255 }).notNull(),
+    name: varchar("name", { length: 255 }),
+    isPro: boolean("is_pro").notNull().default(false),
+    created_at: timestamp("created_at", { withTimezone: true }).defaultNow(),
+});
+
+export const tokens = pgTable("tokens", {
+    id: uuid("id").primaryKey(),
+    userId: uuid("user_id")
+        .notNull()
+        .references(() => users.id),
+    token: varchar("token", { length: 128 }).notNull(),
+    createdAt: timestamp("created_at", { withTimezone: true }).defaultNow(),
+    updatedAt: timestamp("updated_at", { withTimezone: true }).defaultNow(),
+});
+
+export const billing = pgTable("billing", {
+    id: uuid("id").primaryKey(),
+    userId: uuid("user_id")
+        .notNull()
+        .references(() => users.id),
+    amount: numeric("amount").notNull(),
+    currency: varchar("currency", { length: 10 }).notNull(),
+    purchasedAt: timestamp("purchased_at", {
+        withTimezone: true,
+    }).defaultNow(),
+    isEmailSent: boolean("is_email_sent").default(false),
+    createdAt: timestamp("created_at", { withTimezone: true }).defaultNow(),
+    updatedAt: timestamp("updated_at", { withTimezone: true }).defaultNow(),
+});
+
+export const quotes = pgTable("quotes", {
+    id: uuid("id").primaryKey(),
+    userId: uuid("user_id")
+        .notNull()
+        .references(() => users.id),
+    quote: text("quote").notNull(),
+    createdAt: timestamp("created_at", { withTimezone: true }).defaultNow(),
+    updatedAt: timestamp("updated_at", { withTimezone: true }).defaultNow(),
+});
