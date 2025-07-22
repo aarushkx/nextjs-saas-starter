51781e3351434dc477699591956a19066ee692df
Aarush Kumar <2005aarushkumar@gmail.com>
2025-07-22 12:15:51 +0530

    feat: add Clerk auth

---
+++ lib/constants.ts
@@ -0,0 +1,16 @@
+export const APP_NAME = "MySaaS";
+
+export const APP_DESCRIPTION =
+    "A modern, open-source Next.js SaaS starter kit providing a robust foundation for your next software product.";
+
+export const APP_KEYWORDS = [
+    "Next.js",
+    "TypeScript",
+    "Neon",
+    "React",
+    "Tailwind CSS",
+    "Clerk",
+    "Drizzle ORM",
+    "Shadcn UI",
+    "Google Gemini",
+];
---
+++ providers/providers.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import React from "react";
+import { ClerkProvider } from "@clerk/nextjs";
+
+const Providers = ({ children }: { children: React.ReactNode }) => {
+    return <ClerkProvider>{children}</ClerkProvider>;
+};
+
+export default Providers;
